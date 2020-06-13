26a2b2676cddd03c9c701bf0a26c41444112f08c
건웅방 <bgu7421@gmail.com>
2020-06-13 23:43:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/covidapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.covidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView flags;
+    Spinner spinner;
+    Button stat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        stat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(MainActivity.this, Statistics.class);
+                startActivity(i);
+
+            }
+        });
+
+
+    }
+}
