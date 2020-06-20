e9a3fadd6537bc34e1bcc173b7d15cff35a16699
건웅방 <bgu7421@gmail.com>
2020-06-20 21:25:05 +0800

    Initial commit

--- app/src/main/java/com/example/covidapp/MainActivity.java
+++ app/src/main/java/com/example/covidapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.covidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +12,21 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static com.example.covidapp.R.id.googleMap;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+   private FragmentManager fragmentManager;
+   private MapFragment mapFragment;
+
 
     ImageView flags;
     Spinner spinner;
@@ -22,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment)fragmentManager.findFragmentById(googleMap);
+        mapFragment.getMapAsync(this);
 
         stat.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +51,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location = new LatLng(1.360617, 103.827608); // Singapore's location
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("Total Case in Singapore");
+        markerOptions.snippet("40000");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
     }
 }
